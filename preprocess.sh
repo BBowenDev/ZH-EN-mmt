402d3b3c558d89fbe7b5ae8dc69e8343d98bcdb8
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-06-24 16:48:53 -0400

    Create preprocess.sh

---
+++ preprocess.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [ ! -z $1 ]; then
+  MERGES = $1
+else; then
+  MERGES = 10000
+fi
+
+FV=$(pwd)
+SWNMT=$FV/subword-nmt
+VT=$FV/vatex
+TOK=$VT/tok
+RAW=$VT/raw
+BPE=$VT/bpe
+VOC=$VT/vocab
+
+#get raw captions
+echo "Getting raw captions"
+wget "https://eric-xw.github.io/vatex-website/data/vatex_training_v1.0.json" -P $RAW &
+wget "https://eric-xw.github.io/vatex-website/data/vatex_validation_v1.0.json" -P $RAW &
+wait
+
+#run preprocessing script on raw captions, tokenizing and saving to new files
+echo "Tokenizing dataset"
+cd $VT/scripts
+python vatex_preprocess.py -f True -t $MERGES
+
+#10,000 merge operations are used (can be hyperparamaterized)
+#learning and applying bpe are broken up so they can be parallelized
+cd $SWNMT
+echo "Learning BPE:"
+for TYPE in "train" "val" "test"; do
+	for LANG in "en" "zh"; do 
+		INPUT="${TOK}/${TYPE}_tok.${LANG}"
+		OUTPUT="${BPE}/${TYPE}.bpe${MERGES}.${LANG}"
+		CODES="${TOK}/codes_${LANG}.bpe"
+		VOCAB="${VOC}/${TYPE}_vocab.${LANG}"
+
+		echo "--${TYPE}-${LANG}"
+		python $SWNMT/subword_nmt/learn_joint_bpe_and_vocab.py -s $MERGES -o $CODES --input $INPUT --write-vocabulary $VOCAB
+	done
+done
+wait
+
+#once all BPE has been learned, it is applied
+echo "Applying BPE:"
+for TYPE in "train" "val" "test"; do
+	for LANG in "en" "zh"; do 
+		INPUT="${TOK}/${TYPE}_tok.${LANG}"
+		OUTPUT="${BPE}/${TYPE}.bpe${MERGES}.${LANG}"
+		CODES="${TOK}/codes_${LANG}.bpe"
+		VOCAB="${VOC}/${TYPE}_vocab.${LANG}"
+
+		echo "--${TYPE}-${LANG}"
+		python $SWNMT/subword_nmt/apply_bpe.py -c $CODES --vocabulary $VOCAB < $INPUT > $OUTPUT
+	done
+done
+wait
