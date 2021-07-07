41e02e203d3e559707894610a0c373f96c28d2c0
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-07-07 12:23:52 -0400

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -23,7 +23,7 @@ function proc {
 	#run preprocessing script on raw captions, tokenizing and saving to new files
 	echo "Tokenizing dataset"
 	cd $VT/scripts
-	python vatex_preprocess.py -t T -f "True"
+	python vatex_preprocess.py -t $T -f "True"
 
 	#10,000 merge operations are used (can be hyperparamaterized)
 	#learning and applying bpe are broken up so they can be parallelized
