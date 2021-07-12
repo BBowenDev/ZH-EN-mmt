77ef285c3403d4b48f4a616604dfa72d52875de8
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-07-12 11:48:51 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -13,19 +13,19 @@ function download_all {
 function download_select {
 	echo "Downloading ${1} videos"
 	MAX=$1
-	SEEN=0
+	SEEN=1
 	ERR_ALL=0
 	for FILE in "$RAW"/*.ids; do
 		ERR=0
 		while read -r L; do
 			#if the given number of videos has been downloaded, break loop
-			if [[ $SEEN -ge $MAX ]]; then 
+			if [[ $SEEN -gt $MAX ]]; then 
 				echo "Seen a Maximum of ${SEEN} Lines"
 				break
 			fi 
 			#get video file location (e.g. test, val, train)
 			IFS="/" read -r -a FARR <<< $FILE
-			FILE="${FARR[-1]}"
+			F_FILE="${FARR[-1]}"
 
 			#set the string delimiter to "_" to break up each line into an array
 			IFS="_" read -r -a ARR <<< $L
@@ -51,44 +51,51 @@ function download_select {
 				OUT=0
 			fi
 
-			#clip duration is clip end time - clip start time $LN
+			#clip duration is (clip end time $OUT - clip start time $IN) = $LN
 			LN=$((${OUT}-${IN}))
 
 			#set expected file name
-			NAME=$VIDS/"${FILE}.${ID}.mp4"
+			NAME=$VIDS/"${F_FILE}.${ID}.mp4"
 
-			#for every video, download from given timeframe
+			#for every video, download from given time frame
 			YTDL_FAIL=false			
 			echo "Starting Download ${ID}: ${SEEN}/${MAX}"
 
 			#access and download whole video with youtube-dl
 			#youtube-dl -f mp4/bestvideo captures video and audio in the best accessible format
 			#youtube-dl -q shows no output
-			youtube-dl "${ID}" -f mp4/bestvideo --external-downloader ffmpeg -o "${NAME}" || true; YTDL_FAIL=true
+			
+			#youtube-dl "${ID}" -f mp4/bestvideo --external-downloader ffmpeg -o "${NAME}" || true; YTDL_FAIL=true
 
 			#if the download doesn't complete or an error is returned, skip and increment error count
-			if [[ $YTDL_FAIL ]]; then
-				echo "-----------------------------------YT-DL FAILED VIDEO ${ID}"
-				((ERR+=1))
-			else 
+			if (youtube-dl "${ID}" -f mp4/bestvideo --external-downloader ffmpeg -o $NAME); then
 				echo "-----------------------------------YT-DL DOWNLOADED VIDEO ${ID}"
-
 				FF_FAIL=false
 
 				#trim and encode video clip
 				#if the encoding doesn't complete or an error is returned, skip and increment error count
 				#ffmpeg -loglevel 8 only shows errors that break the download process
-				ffmpeg -ss $IN -t $LN -i $NAME || true; FF_FAIL=true
+				
+				#ffmpeg -ss $IN -t $LN -i $NAME -c:v copy -c:a copy $NAME || true; FF_FAIL=true
 
-
-				if [[ $FF_FAIL ]]; then 
-					((ERR+=1))
-					echo "-----------------------------------FFMPEG FAILED VIDEO ${ID}"
-				else
+				if (ffmpeg -ss $IN -t $LN -i $NAME -c:v copy -c:a copy $NAME); then 
 					echo "-----------------------------------FFMPEG TRIMMED VIDEO ${ID}"
 					((SEEN+=1))
+				else
+					((ERR+=1))
+					echo "-----------------------------------FFMPEG FAILED VIDEO ${ID}"
+					
+
 				fi	
 			fi
+			
+			else 
+				echo "-----------------------------------YT-DL FAILED VIDEO ${ID}"
+				((ERR+=1))
+				
+				
+
+				
 					
 		done < $FILE
 		echo "--Videos Downloaded in ${FILE}: ${SEEN}"
