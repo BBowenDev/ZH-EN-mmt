c247f37267cc4a363ae5b904567dbc3bf7f9aba3
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-07-13 12:59:35 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -170,7 +170,6 @@ function download_random {
 	while [[ $SEEN -lt $MAX ]]; do
 		#get random line from selected file
 		END=$(cat $RAW/$I_FILE | wc -l)
-		echo "${END}"
 		RAND=$(shuf -i 0-$END -n 1)
 		LN=$(sed "${RAND}q;d" "${RAW}/${I_FILE}")
 		
