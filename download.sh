ac1dfff3b0bda3fb950a67c2f37aa443daeaee7a
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-07-19 14:00:40 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,9 +8,59 @@ function show_help {
 	echo "Usage: download.sh -arg val"
 	echo "Usage: download.sh"
 	echo "--Use -a to fetch all available videos from each set (or use no arguments)"
-	echo "--Use -s <n> to fetch the first n available videos from the test set"
+	echo "--Use -t <n> to fetch the first n available videos from the test set"
+	echo "--Use -s <train/val/test> to fetch all available videos from the selected set"
 	echo "--Use -r <train/val/test> to fetch a random available video from the selected set"
 	exit 0
+}
+
+#performs the download, trim, and encode function for all iterations of download.sh
+function download {
+	FILE=$1
+	OUTDIR=$2
+	L=$3
+	
+	#set the string delimiter to "_" to break up each line into an array
+	IFS="=" read -r -a ARR <<< $L
+	
+	#set video ID
+	ID=${ARR[0]}
+	#set clip start time $IN
+	IN=${ARR[1]}
+	#set clip duration $DR
+	DR=${ARR[2]}
+
+	#set full video download name
+	NAME=$VIDS/"${FILE}.${ID}.mp4"
+	#set trimmed video download name
+	SVNAME=$OUTDIR/"${FILE}.${ID}.mp4"
+	
+	#if the file already exists, skip the download
+	if [[ ! -f "$SVNAME"  ]]; then
+		#access and download whole video with youtube-dl
+		#youtube-dl -f mp4/bestvideo captures video and audio in the best accessible format
+		#youtube-dl -q shows no output
+		
+		#if the download doesn't complete or an error is returned, skip and increment error count
+		if (youtube-dl -q -i -f mp4/bestvideo --external-downloader ffmpeg -o $NAME "https://www.youtube.com/watch?v=${ID}"); then
+
+			#trim and encode video clip
+			#ffmpeg -nostdin prevents reading from STDIN, which causes errors with $ bash read
+			#ffmpeg -loglevel 8 only shows errors that break the download process
+
+			#if the encoding doesn't complete or an error is returned, skip and increment error count
+			if (ffmpeg -nostdin -loglevel 8 -ss $IN -t $DR -i $NAME -c:v copy -c:a copy -y $SVNAME); then 
+
+				#remove unencoded file
+				rm $NAME
+				return 0
+			else
+				#remove unencoded file
+				rm $NAME
+				return 1
+		else
+			return 1
+	fi
 }
 
 function download_all {
@@ -32,46 +82,13 @@ function download_all {
 		OUTDIR=$RAW/"${FILE}.vids"
 		
 		while read -r L; do
-			#set the string delimiter to "_" to break up each line into an array
-			IFS="=" read -r -a ARR <<< $L
-			
-			#set video ID
-			ID=${ARR[0]}
-			#set clip start time $IN
-			IN=${ARR[1]}
-			#set clip duration $DR
-			DR=${ARR[2]}
-
-			#set full video download name
-			NAME=$VIDS/"${FILE}.${ID}.mp4"
-			#set trimmed video download name
-			SVNAME=$OUTDIR/"${FILE}.${ID}.mp4"
-
-			#access and download whole video with youtube-dl
-			#youtube-dl -f mp4/bestvideo captures video and audio in the best accessible format
-			#youtube-dl -q shows no output
-			
-			#if the download doesn't complete or an error is returned, skip and increment error count
-			if (youtube-dl "${ID}" -q -f mp4/bestvideo --external-downloader ffmpeg -o $NAME); then
-
-				#trim and encode video clip
-				#ffmpeg -nostdin prevents reading from STDIN, which causes errors with $ bash read
-				#ffmpeg -loglevel 8 only shows errors that break the download process
-				
-				#if the encoding doesn't complete or an error is returned, skip and increment error count
-				if (ffmpeg -nostdin -loglevel 8 -ss $IN -t $DR -i $NAME -c:v copy -c:a copy -y $SVNAME); then 
-					
-					#remove unencoded file
-					rm $NAME
-					((SEEN+=1))
-					echo "${SEEN}: Downloaded Video ${SVNAME}"
-				else
-					((ERR+=1))
-				fi	
+			if [[ download $FILE $OUTDIR $L ]]; then 
+				((SEEN+=1))
+				echo "Downloaded Video ${SEEN} at ${OUTDIR}"
 			else 
 				((ERR+=1))
 			fi
-					
+				
 		done < $F_FILE
 		echo "--Videos Downloaded in ${FILE}: ${SEEN}"
 		echo "--Videos Skipped in ${FILE}: ${ERR}"
@@ -105,53 +122,38 @@ function download_select {
 			break
 		fi 
 
-		#set the string delimiter to "_" to break up each line into an array
-		IFS="=" read -r -a ARR <<< $L
-
-		#set video ID
-		ID=${ARR[0]}
-		#set clip start time $IN
-		IN=${ARR[1]}
-		#set clip duration $DR
-		DR=${ARR[2]}
-
-		#set full video download name
-		NAME=$VIDS/"${FILE}.${ID}.mp4"
-		#set trimmed video download name
-		SVNAME=$OUTDIR/"${FILE}.${ID}.mp4"
-
-		#for every video, download from given time frame	
-		echo "Starting Download ${ID}: ${SEEN}/${MAX}"
-
-		#access and download whole video with youtube-dl
-		#youtube-dl -f mp4/bestvideo captures video and audio in the best accessible format
-		#youtube-dl -q shows no output
-
-		#if the download doesn't complete or an error is returned, skip and increment error count
-		if (youtube-dl "${ID}" -q -f mp4/bestvideo --external-downloader ffmpeg -o "${NAME}"); then
-
-			#trim and encode video clip
-			#ffmpeg -nostdin prevents reading from STDIN, which causes errors with $ bash read
-			#ffmpeg -loglevel 8 only shows errors that break the download process
-
-			#if the encoding doesn't complete or an error is returned, skip and increment error count
-			if (ffmpeg -nostdin -loglevel 8 -ss "${IN}" -t "${DR}" -i "${NAME}" -c:v copy -c:a copy -y "${SVNAME}"); then 
-				#remove unencoded file
-				rm $NAME
-				((SEEN+=1))
-			else
-				((ERR+=1))
-			fi	
+		if [[ download $FILE $OUTDIR $L ]]; then 
+			((SEEN+=1))
+			echo "${SEEN}/${MAX}: Downloaded Video ${SVNAME}"
 		else 
 			((ERR+=1))
 		fi
-
 	done < $RAW/"${I_FILE}"
 	if [[ $SEEN -gt $MAX ]]; then 
 		echo "Seen a Maximum of ${SEEN} Lines"
 		echo "--Videos Downloaded to ${FILE}: ${SEEN}"
 		echo "--Videos Skipped in ${FILE}: ${ERR}"
 	fi
+}
+
+function download_set {
+	echo "Downloading all available videos from set ${1}"
+	FILE="${1}.ids"
+	
+	SEEN=0
+		
+	if [[ ! -d $RAW/"${FILE}.vids" ]]; then
+		mkdir $RAW/"${FILE}.vids"
+	fi
+	#target output folder to storage
+	OUTDIR=$RAW/"${FILE}.vids"
+
+	while read -r L; do
+		if [[ download $FILE $OUTDIR $L ]]; then 
+			((SEEN+=1))
+			echo "Downloaded Random Video at ${OUTDIR}"
+		fi
+	done < $FILE
 }
 
 function download_random {
@@ -174,39 +176,9 @@ function download_random {
 		RAND=$(shuf -i 0-$END -n 1)
 		LN=$(sed "${RAND}q;d" "${RAW}/${I_FILE}")
 		
-		#set the string delimiter to "_" to break up each line into an array
-		IFS="=" read -r -a ARR <<< $LN
-		
-		#set video ID
-		ID=${ARR[0]}
-		#set clip start time $IN
-		IN=${ARR[1]}
-		#set clip duration $DR
-		DR=${ARR[2]}
-
-		#set full video download name
-		NAME=$VIDS/"${FILE}.${ID}.mp4"
-		#set trimmed video download name
-		SVNAME=$OUTDIR/"${FILE}.${ID}.mp4"
-
-		#access and download whole video with youtube-dl
-		#youtube-dl -f mp4/bestvideo captures video and audio in the best accessible format
-		#youtube-dl -q shows no output
-
-		#if the download doesn't complete or an error is returned, skip and increment error count
-		if (youtube-dl "${ID}" -q -f mp4/bestvideo --external-downloader ffmpeg -o "${NAME}"); then
-
-			#trim and encode video clip
-			#ffmpeg -nostdin prevents reading from STDIN, which causes errors with $ bash read
-			#ffmpeg -loglevel 8 only shows errors that break the download process
-
-			#if the encoding doesn't complete or an error is returned, skip and increment error count
-			if (ffmpeg -nostdin -loglevel 8 -ss "${IN}" -t "${DR}" -i "${NAME}" -c:v copy -c:a copy -y "${SVNAME}"); then 
-				#remove unencoded file
-				rm $NAME
-				((SEEN+=1))
-				echo "Downloaded Random Video ${NAME}"
-			fi
+		if [[ download $FILE $OUTDIR $LN ]]; then 
+			((SEEN+=1))
+			echo "Downloaded Video at ${OUTDIR}"
 		fi
 	done	
 }
@@ -223,13 +195,22 @@ else
 			-a) # download all videos
 				download_all
 				;;
-			-s) #select the first n available videos from the test set
+			-s) #download all videos from chosen set 
+				shift
+				if test $# -gt 0; then 
+					download_set $1
+					shift
+				else
+					echo "Error in arg -s: <train/val/test> set to fetch all videos from"
+				fi
+				
+			-t) #select the first n available videos from the test set
 				shift
 				if test $# -gt 0; then 
 					download_select $1
 					shift
 				else 
-					echo "Error in arg -s: <int> num videos r"
+					echo "Error in arg -t: <int> num videos r"
 					exit 0
 				fi
 				;;
